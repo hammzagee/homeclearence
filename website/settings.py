844f04ab3c6ac7aaa0f26fec1102951742fe6148
Haaamza <hamzagee@outlook.com.com>
2020-02-25 00:27:08 +0500

    db set

--- website/settings.py
+++ website/settings.py
@@ -86,14 +86,20 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.mysql',
+    #     'NAME': 'safi',
+    #     'USER': 'root',
+    #     'PASSWORD': 'root',
+    #     'HOST': 'localhost',
+    #     'PORT': '3306',
+    # }
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'safi',
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'USER': 'b24e9f4bae18e4',
+        'PASSWORD': '0bf30d6c',
+        'HOST': 'eu-cdbr-west-02.cleardb.net',  
         'PORT': '3306',
     }
 }
